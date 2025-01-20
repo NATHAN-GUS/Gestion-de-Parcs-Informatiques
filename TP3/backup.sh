35e039b94a8eb39582e39f3b67dc137e55e3aaa1
NATHAN-GUS <nnathanhoamb@gmail.com>
2025-01-20 15:06:52 +0100

    Update backup.sh

--- TP3/backup.sh
+++ TP3/backup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-echo " Win win script lanceu"
-tar -czvf /mnt/music_backup/music_$(date +"%y%m%d_%H%M%S").tar.gz -C /srv/music
-echo " Music telehr succes : music_$(date +"%y%m%d_%H%M%S").tar.gz"
 
+# SOURCE="/srv/music"
+BACKUP_DIR="/mnt/music_backup"
+LOG=$(date +"%y%m%d_%H%M%S")
+NAME_ARCH="music_${LOG}.tar.gz"
+
+tar -czf "$BACKUP_DIR/$NAME_ARCH" -C "$SOURCE" .
+
+if [[ $? -eq 0 ]]; then
+    echo "Sauvegarde réussie : $BACKUP_DIR/$ARCHIVE_NAME"
+else
+    echo "Erreur lors de la création de la sauvegarde."
+fi
