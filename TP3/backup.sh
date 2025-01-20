b727e7222d5795be35762aa03afc000cf5595f7a
NATHAN-GUS <nnathanhoamb@gmail.com>
2025-01-20 14:27:10 +0100

    Update backup.sh

--- TP3/backup.sh
+++ TP3/backup.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+echo " Win win script lanceu"
+tar -czvf /mnt/music_backup/music_$(date +"%y%m%d_%H%M%S").tar.gz -C /srv/music
+echo " Music telehr succes : music_$(date +"%y%m%d_%H%M%S").tar.gz"
 
